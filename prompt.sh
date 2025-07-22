d3d9aff7cbfab28ae63b5d763088e472572b3413
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-07-22 11:01:52 -0400

    prompt

---
+++ prompt.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+#SBATCH --job-name=knowledge_graph_training
+#SBATCH --output=output/output.out
+#SBATCH --mem=32G
+#SBATCH --gres=gpu:1
+
+source ~/.bashrc
+conda activate ecause
+
+cd /home/hgao62/Position-Bias-Mitigation-in-Emotion-Cause-Analysis
+
+# GPU_ID="0"
+# MODELS="Qwen/Qwen2.5-7B,meta-llama/Llama-3.1-8B,deepseek-ai/DeepSeek-R1-Distill-Qwen-7B,deepseek-ai/DeepSeek-R1-Distill-Llama-8B"
+
+# MODELS="Qwen/Qwen2.5-7B,meta-llama/Llama-3.1-8B"
+
+# PROMPT_FILE="prompts.txt"
+# MAX_NEW_TOKENS=1024
+
+python ad_paedgl.py
+    # --gpu_id $GPU_ID \
+    # --models $MODELS \
+    # --prompt_file $PROMPT_FILE \
+    # --max_new_tokens $MAX_NEW_TOKENS

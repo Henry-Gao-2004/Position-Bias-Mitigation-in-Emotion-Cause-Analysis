6b30cb6be3188261f7df0b06ef016060f1378ee2
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-07-27 15:58:48 -0400

    Update ad_paedgl.py

--- ad_paedgl.py
+++ ad_paedgl.py
@@ -3,6 +3,9 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+
 import time
 import numpy as np
 import tensorflow as tf

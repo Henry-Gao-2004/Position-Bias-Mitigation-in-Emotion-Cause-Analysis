f001299eacc1f9f3373d021543b3d07822eb1019
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-07-30 16:28:33 -0400

    Update ad_paedgl.py

--- ad_paedgl.py
+++ ad_paedgl.py
@@ -261,8 +261,8 @@ def train_adv(model=None):
                     raw_sentence = train_doc_data.copy()
                     '''Step2: warm up the discriminator'''
                     if this_global_step < FLAGS.dis_warm_up_step:
-                        print("Shape of train_doc_data:", train_doc_data.shape)
-                        print("Shape of train_sen_len_data:", train_sen_len_data.shape)
+                        for tensor in [train_doc_data, train_sen_len_data, train_label_data, train_word_dis_data, train_DGL_data, train_doc_len_data, train_label_p_data]:
+                            print(f"Tensor {tensor.name} shape: {tensor.shape}")
                         dis_loss, _,= sess.run([dis_loss_op, train_dis_op],feed_dict={
                             'discriminator/train_doc:0': train_doc_data,
                             'discriminator/train_sen_len:0': train_sen_len_data,

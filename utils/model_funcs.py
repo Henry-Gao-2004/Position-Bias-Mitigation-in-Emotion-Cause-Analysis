1610cb0f223fe469c0939e70fa58ee18c7f6433e
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-08-26 17:40:39 -0400

    Update model_funcs.py

--- utils/model_funcs.py
+++ utils/model_funcs.py
@@ -102,14 +102,13 @@ def biLSTM(inputs, length, n_hidden, scope):
     length shape:[batch_size]
     return shape:[batch_size, max_len, n_hidden*2]
     '''
-    outputs, state = tf.nn.bidirectional_dynamic_rnn(
-        cell_fw=tf.contrib.rnn.LSTMCell(n_hidden),
-        cell_bw=tf.contrib.rnn.LSTMCell(n_hidden),
-        inputs=inputs,
-        sequence_length=length,
-        dtype=tf.float32,
-        scope=scope
-    )
+    lstm_fw = tf.keras.layers.LSTM(n_hidden, return_sequences=True, return_state=True, go_backwards=False)
+    lstm_bw = tf.keras.layers.LSTM(n_hidden, return_sequences=True, return_state=True, go_backwards=True)
+
+    outputs_fw, _, _ = lstm_fw(inputs, mask=tf.sequence_mask(length))
+    outputs_bw, _, _ = lstm_bw(inputs, mask=tf.sequence_mask(length))
+
+    outputs = (outputs_fw, outputs_bw)
 
     return tf.concat(outputs, 2)
 

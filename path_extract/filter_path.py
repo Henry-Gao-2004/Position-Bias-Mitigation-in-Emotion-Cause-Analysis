6a93e9ffd09836014e9447e14a3b119fdc0965eb
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-08-26 15:58:46 -0400

    Update filter_path.py

--- path_extract/filter_path.py
+++ path_extract/filter_path.py
@@ -130,7 +130,7 @@ def en2ch_path(en_path):
         idx +=1
     return path
 
-path_file = open("path_data_1120_num15.txt",'w')
+path_file = open("path_data_1120_num15.txt",'wb')
 
 filtered_paths = []
 for paths in path_data_list:

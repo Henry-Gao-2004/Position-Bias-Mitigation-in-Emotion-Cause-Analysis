a4eb98c47657941241d484d1d4ee93eab000d2eb
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-08-28 18:59:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -259,6 +259,7 @@ def run():
                 valid_loss = []
                 # best_pre = []
                 for train, _ in get_batch_data(tr_x, tr_y, tr_sen_len, tr_doc_len, tr_word_dis, tr_adj_data, tr_emotion_pos,tr_path, tr_path_num, tr_path_len, FLAGS.keep_prob1, FLAGS.keep_prob2, FLAGS.batch_size):
+                    print(train.shape)
                     _ = sess.run(optimizer,feed_dict=dict(zip(placeholders, train)))
                     loss = sess.run(loss_op,feed_dict=dict(zip(placeholders, train)))
                     pred_y = sess.run(pred_y_op,feed_dict=dict(zip(placeholders, train)))

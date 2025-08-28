6122f5492e8d77f5fe59ae4333e3c7f088115333
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-08-28 10:51:38 -0400

    Update main.py

--- main.py
+++ main.py
@@ -216,6 +216,7 @@ def run():
         for train, test in kf.split(x_data):
             split_num += 1
             print("Split:%d/10"%split_num)
+            train = np.array(train, dtype=int)
             print(type(train))
             tr_x, tr_y, tr_sen_len, tr_doc_len, tr_word_dis, tr_adj_data, tr_emotion_pos, tr_path, tr_path_num, tr_path_len = map(lambda x: x[train],
             [x_data, y_data, sen_len_data, doc_len_data, word_distance, adj, emotion_pos_data, path_data, path_num_data, path_len_data])

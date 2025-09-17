d0a35a95b11cd38c7c7c3303c26fe59680773982
Henry Gao <77124321+Henry-Gao-2004@users.noreply.github.com>
2025-09-16 20:19:00 -0400

    Update main.py

--- main.py
+++ main.py
@@ -312,7 +312,8 @@ def run():
                     print(train[6].shape)
                     train[7] = pad3d_str_np(train[7])
                     train[7] = words_to_ids(train[7]) #[96,10,7]
-                    # train[7] = tf.reshape(train[7], [-1, 75, 10, 7])
+                    train[7] = tf.reshape(train[7], [-1, 1, 10, 7])
+                    train[7] = tf.tile(train[7], [64, 75, 10, 7])
                     train[7] = sess.run(train[7]).tolist()
                     # placeholders = [x, y, sen_len, doc_len, word_dis, adj_tensor, emotion_pos,path_data_op, path_num_op, path_len_op, keep_prob1, keep_prob2]
                     _, loss, pred_y, true_y, pred_prob, doc_len_batch,_ = sess.run(
